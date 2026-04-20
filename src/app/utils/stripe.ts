7548d88a3c0d884a5bc98433e0f17788ea1baaea
gazi275 <gaziazad270@gmail.com>
2026-04-21 01:50:21 +0600

    chore: update environment variables and remove unused Stripe configuration

--- src/app/utils/stripe.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe"
-import config from "../../config"
-
-const stripe = new Stripe(config.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
-})
-
-export { stripe }
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,7 +5,6 @@ dotenv.config({ path: path.join(process.cwd(), ".env") })
 
 export default {
   env: process.env.NODE_ENV,
-  stripe_key: process.env.STRIPE_SECRET_KEY,
   port: process.env.PORT,
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   jwt: {
@@ -20,11 +19,4 @@ export default {
   email: process.env.EMAIL,
   brevo_email: process.env.EMAIL,
   brevo_pass: process.env.BREVO_PASS,
-
-  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-
-  DO_SPACES_ENDPOINT: process.env.DO_SPACE_ENDPOINT,
-  DO_ACCESS_KEY_ID: process.env.DO_SPACE_ACCESS_KEY,
-  DO_SECRET_ACCESS_KEY: process.env.DO_SPACE_SECRET_KEY,
-  DO_SPACES_BUCKET: process.env.DO_SPACE_BUCKET,
 }
